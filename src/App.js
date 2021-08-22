046dc92bc176f53f02088aab5d57bdcc0e131a03
Justin Ochoa <just.och92@gmail.com>
2021-08-22 12:23:22 -0400

    fixed title double render and got both inputs to work

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Axios from "axios";
 import "./App.css";
 
 function App() {
-  const [addTitle, setAddTitle] = useState("");
+  const initialValues = {
+    title: "",
+    description: "",
+  };
+  const [values, setValues] = useState(initialValues);
   const [addItem, setAddItem] = useState([]);
   const [notes, setNotes] = useState([]);
 
@@ -14,14 +18,15 @@ function App() {
   };
 
   const handleChange = (e) => {
-    setAddTitle(e.target.value);
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const res = await Axios.post("http://localhost:3001/notes", {
-      title: addTitle,
-      description: "testing",
+      title: values.title,
+      description: values.description,
     });
     setAddItem(res.data);
   };
@@ -29,7 +34,7 @@ function App() {
   const renderNotes = () => {
     return notes.map((note, index) => {
       return (
-        <div key={index}>
+        <div className="container" key={index}>
           <div className="card m-3">
             <h5 className="card-header">{note.title}</h5>
             <div className="card-body">
@@ -48,27 +53,43 @@ function App() {
   return (
     <div className="App">
       <h1>Notes</h1>
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text" id="inputGroup-sizing-default">
-            Add Title
-          </span>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="inputGroup-sizing-default">
+              Add Title
+            </span>
+          </div>
         </div>
-        <form onSubmit={handleSubmit}>
-          <input
-            onChange={handleChange}
-            name="title"
-            type="text"
-            className="form-control"
-            aria-label="Default"
-            aria-describedby="inputGroup-sizing-default"
-          />
-        </form>
-      </div>
-      {addItem.title}
+        <input
+          onChange={handleChange}
+          name="title"
+          value={values.title}
+          type="text"
+          className="form-control mb-3"
+          aria-label="Default"
+          aria-describedby="inputGroup-sizing-default"
+        />
+        <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="inputGroup-sizing-default">
+              Add Description
+            </span>
+          </div>
+        </div>
+
+        <input
+          onChange={handleChange}
+          name="description"
+          value={values.description}
+          type="text"
+          className="form-control mb-2"
+          aria-label="Default"
+          aria-describedby="inputGroup-sizing-default"
+        />
+        <button className="btn btn-outline-primary">Add</button>
+      </form>
       {renderNotes()}
-      {/* <Input /> */}
-      {/* <Notes /> */}
     </div>
   );
 }
